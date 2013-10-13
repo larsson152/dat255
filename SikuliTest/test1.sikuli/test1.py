2fb0bbb847315e79971aed0b192e2c321515f15d
andreas <adde_300@hotmail.com>
2013-10-13 16:21:40 +0200

    added sikuli test and testlevel that is level3

---
+++ SikuliTest/test1.sikuli/test1.py
@@ -0,0 +1,67 @@
+wait("1381664030750.png",25)
+click("1381664030750.png")
+wait("1381664150322.png",20)
+type("test")
+click("ok.png")
+wait("1381664344592.png",30)
+click("1381664344592.png")
+wait("playerstart.png",15)
+while(1):
+    hover("1381190420329.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp()
+    wait(1.5)
+    if exists("1381190753399.png") :       
+        break
+while(1):
+    hover("1381190420329.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp()
+    wait(1.5)
+    if exists("1381190808629.png") :
+        break
+while(1):
+    hover("1381190849681.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp()
+    wait(1.5)
+    if exists("1381190870019.png") :
+        break      
+while(1):
+    hover("1381190849681.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp()
+    wait(1.5)
+    if exists("1381190906132.png") :
+        break
+while(1):
+    hover("1381190849681.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp()
+    wait(1.5)
+    if exists("1381190939601.png") :
+        break 
+while(1):
+    hover("1381191199148.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp()
+    wait(3)
+    if exists("1381664779913.png") :
+        break 
+while(1):
+    hover("1381664800176.png")
+    mouseDown(Button.LEFT) 
+    wait(0.15)
+    mouseUp() 
+    wait(6)
+    if exists("1381664344592.png") :
+        break     
+
+
+
